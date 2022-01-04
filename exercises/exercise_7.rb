82695078f4858ea4bdc13a54171509446c0da9bc
Walid <walid_douri@hotmail.com>
2022-01-04 18:55:48 -0500

    Exercise 7: Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,39 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+#1.
+
+# employees
+# are we updating employees?
+# employees.string :first_name, null: false
+# employees.string :last_name, null: false
+# employees.integer :hourly_rate: (40..200), null: false 
+# employees.belongs_to :store
+
+# store
+# store.string :name (3..200), null: false
+# store.integer :annual_revenue, null: false #set a default of 0? set min?
+
+#2.
+
+def print_question
+  puts "what would you like to name the store?"
+end
+
+puts print_question
+store_name = gets.chomp.to_s
+
+#3.
+
+store = Store.new(name: store_name)
+store.save
+puts store.errors.messages
+
+#4
+
+
+# if statements/ create method ???
+# puts "Sorry! The store name must be more than three characters"
+# puts "Sorry! The annual_revenue must be greater than $0"
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :apperal_validation
+
+  def apperal_validation
+      if (mens_apparel == false && womens_apparel == false) || (mens_apparel == nil && womens_apparel == nil)
+        errors.add(:mens_apparel, "You must carry at least mens or womens apperal")
+        errors.add(:womens_apparel, "You must carry at least mens or womens apperal")
+      end
+  end
 end
