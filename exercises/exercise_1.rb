1786776a6f2ed7ece88bd3d5fae4a56044708623
Walid <walid_douri@hotmail.com>
2022-01-04 12:41:08 -0500

    Exercise 1: Create 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,4 +5,49 @@ puts "----------"
 
 # Your code goes below here ...
 
-puts "What happens"
+# ActiveRecord::Schema.define do
+#   create_table :stores do |s|
+#     s.id :primary_key, null: false
+#     s.string :name, null: false
+#     s.integer :annual_revenue, null: false
+#     s.boolean :mens_apparel, null: false
+#     s.boolean :womens_apparel, null: false
+# end
+
+# # CREATE TABLE stores (
+# #   id SERIAL PRIMARY KEY NOT NULL,
+# #   name varchar(255),
+# #   annual_revenue INTEGER NOT NULL,
+# #   mens_apparel BOOLEAN NOT NULL,
+# #   womens_apparel BOOLEAN NOT NULL
+# # );
+
+store = Store.new 
+  # s.id = 1
+  store.name = 'Burnaby'
+  store.annual_revenue = 300000
+  store.mens_apparel = true
+  store.womens_apparel = true
+
+  store.save
+
+store = Store.new 
+  # s.id = 1
+  store.name = 'Richmond'
+  store.annual_revenue = 1260000
+  store.mens_apparel = false
+  store.womens_apparel = true
+
+  store.save
+
+store = Store.new 
+  # s.id = 1
+  store.name = 'Gastown'
+  store.annual_revenue = 190000
+  store.mens_apparel = true
+  store.womens_apparel = false
+
+store.save
+# stores.calculate(:count, :all)
+
+
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,21 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+class Store
+
+  def initialize(store1)
+  @store1 = store1
+
+  end
+end
+
+
+store1 = Store.find_by(name: 'Burnaby')
+store1.update(name: 'Toronto')
+
+
+# person = Person.new("Ada")
+# p person
+
+# https://api.rubyonrails.org/classes/ActiveRecord/Schema.html
